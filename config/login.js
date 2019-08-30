aa26146fbc00e7f860b3e38b9af8fd19c3ceae85
unknown <mendez.andrewm@gmail.com>
2019-08-29 19:18:16 -0700

    broke things

---
+++ config/login.js
@@ -0,0 +1,22 @@
+// // var mysql = require('mysql');
+// var express = require('express');
+// // var session = require('express-session');
+// // var bodyParser = require('body-parser');
+// // var path = require('path');
+
+// // const bcrypt = require('bcrypt');
+// const saltRounds = 10;
+// const myPlaintextPassword = 's0/\/\P4$$w0rD';
+// const someOtherPlaintextPassword = 'not_bacon';
+
+// // //put following code into controller folder:
+// // //sync
+// const bcrypt = require('bcrypt');
+// const saltRounds = 10;
+// // Store hash in your password DB.
+// var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+
+// // Load hash from your password DB.
+// bcrypt.compareSync(myPlaintextPassword, hash); // true
+// bcrypt.compareSync(someOtherPlaintextPassword, hash); // false
+// //end controller folder code
