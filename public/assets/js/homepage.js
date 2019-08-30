58baed0c6f4184e8627666d9cafce5c31bef5c5b
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-08-29 21:26:59 -0700

    client side post code written not tested

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,38 +1,97 @@
-    document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
+ 
 
   // Or with jQuery
-
   $(document).ready(function(){
+    // let newTaskUser;
+    // let newTask;
+    // let newTaskFreq ;
+
+
+    // $("#submit-btn").on("click", function(){
+    //   alert("zeb you suck")
+    //    newTaskUser = $("#user-name").val();
+    //    console.log(newTaskUser)
+    //    newTask = $("#user-task").val();
+    // console.log(newTask)
+    //   newTaskFreq = $("#user-frequency").val();
+    //   console.log(newTaskFreq)
+    // });
+    $("#submit-btn").on("click", insertNewTask);
+    
+
+    function insertNewTask(event){
+      event.preventDefault();
+      const task ={
+        user_code: $("#user-name").val().trim(),
+        tasks:  $("#user-task").val().trim(),
+        task_frequency: $("#user-frequency").val().trim(),
+        complete: false
+      }
+      $.post("/", task);
+      $("#user-name").val("");
+      $("#user-task").val("");
+      $("#user-frequency").val("");
+    }
+    }
+     
+    
+
+
+
+
+
+  //   let userName = $("#user-name");
+  //   let userTask = $("#user-task");
+  //   let userFrequency = $("#user-frequency")
+
+  
+  
+  
+  
+  //   const url = window.location.search;
+  //   let taskId;
+  //   if (url.indexOf("?task_id=") !== -1) {
+  //     taskId = url.split("=")[1];
+  //     getTasks(taskId);
+  //   }
+  //   // If there's no authorId we just get all posts as usual
+  //   else {
+  //     getTasks();
+  //   }
+  
+  //   function getTasks(task) {
+  //     taskId = task || "";
+  //     if (taskId) {
+  //       taskId = "/?task_id=" + taskId;
+  //     }
+  //     $.get("/" + taskId, function(data) {
+  //       console.log("Tasks", data);
+  //       tasks = data;
+  //       if (!tasks || !tasks.length) {
+  //         displayEmpty(task);
+  //       }
+  //       else {
+  //         initializeRows();
+  //       }
+  //     });
+  //   }
+  //   function initializeRows() {
+  //     blogContainer.empty();
+  //     var postsToAdd = [];
+  //     for (var i = 0; i < posts.length; i++) {
+  //       postsToAdd.push(createNewRow(posts[i]));
+  //     }
+  //     blogContainer.append(postsToAdd);
+  //   }
+  
+
+    /////////////////carousel//////////////////
+  
+   
     $('.carousel').carousel();
-  });
+
 
   
-  var instance = M.Carousel.getInstance(elem);
-
-  /* jQuery Method Calls
-    You can still use the old jQuery plugin method calls.
-    But you won't be able to access instance properties.
-
-    $('.carousel').carousel('methodName');
-    $('.carousel').carousel('methodName', paramName);
-  */
-
-  
-instance.next();
-instance.next(3); // Move next n times.
-
-instance.prev();
-instance.prev(3); // Move previous n times.
 
 
-instance.set();
-instance.set(3); // Set to nth slide.
-      
-
-instance.destroy();
-      
-
-      
+});
