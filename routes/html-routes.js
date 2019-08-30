d91e004ee44e18224f1c21e3c44b5b507a985b09
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-08-29 19:13:47 -0700

    connected to database/server/homepage

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,9 +3,8 @@ const path = require("path");
 module.exports = function(app){
 
     app.get("/", function(req, res) {
-
         
-        res.sendFile(path.join(__dirname, "../public/blog.html"));
+        res.sendFile(path.join(__dirname, "../public/homepage.html"));
       });
 
       app.get("/cms", function(req, res) {
