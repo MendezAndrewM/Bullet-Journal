2df98b532f9145a08ab770af774c6e8bf09e1987
unknown <mendez.andrewm@gmail.com>
2019-09-03 21:13:15 -0700

    Updated HTML Routing

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -3,7 +3,10 @@ const path = require("path");
 module.exports = function (app) {
 
   app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/welcome.html"));
+  });
 
+  app.get("/home", function (req, res) {
     res.sendFile(path.join(__dirname, "../public/homepage.html"));
   });
 
