e0844ee8a6d26d44de665e2c930a1b8af3e8330f
Zebadiah Miller <Zebadiahmiller@homtial.com>
2019-09-03 20:36:30 -0700

    getting data and able to post data

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,40 +1,80 @@
- 
-
-  // Or with jQuery
-  $(document).ready(function(){
-    // let newTaskUser;
-    // let newTask;
-    // let newTaskFreq ;
 
 
-    // $("#submit-btn").on("click", function(){
-    //   alert("zeb you suck")
-    //    newTaskUser = $("#user-name").val();
-    //    console.log(newTaskUser)
-    //    newTask = $("#user-task").val();
-    // console.log(newTask)
-    //   newTaskFreq = $("#user-frequency").val();
-    //   console.log(newTaskFreq)
-    // });
-    $("#submit-btn").on("click", insertNewTask);
-    
+// Or with jQuery
+$(document).ready(function () {
 
-    function insertNewTask(event){
-      event.preventDefault();
-      const task ={
-        user_code: $("#user-name").val().trim(),
-        tasks:  $("#user-task").val().trim(),
-        task_frequency: $("#user-frequency").val().trim(),
-        complete: false
-      }
-      $.post("/", task);
-      $("#user-name").val("");
-      $("#user-task").val("");
-      $("#user-frequency").val("");
+  const taskContainer = $(".task-container");
+  // let newTaskUser;
+  // let newTask;
+  // let newTaskFreq ;
+
+
+  // $("#submit-btn").on("click", function(){
+  //   alert("zeb you suck")
+  //    newTaskUser = $("#user-name").val();
+  //    console.log(newTaskUser)
+  //    newTask = $("#user-task").val();
+  // console.log(newTask)
+  //   newTaskFreq = $("#user-frequency").val();
+  //   console.log(newTaskFreq)
+  // });
+  let tasks = []
+  function getTasks() {
+    $.get("/api/tasks", function (data) {
+      tasks = data;
+      console.log(tasks)
+      initializeTaskRows();
+    })
+  }
+
+
+
+  $("#submit-btn").on("click", insertNewTask);
+
+
+  function insertNewTask(event) {
+    event.preventDefault();
+    const task = {
+      user_code: $("#user-name").val().trim(),
+      tasks: $("#user-task").val().trim(),
+      task_frequency: $("#user-frequency").val().trim(),
+      complete: false
     }
-    
-     
-    
+    $.post("/", task);
+    $("#user-name").val("");
+    $("#user-task").val("");
+    $("#user-frequency").val("");
+  };
+
+  function initializeTaskRows() {
+    taskContainer.empty();
+    const rowsToAdd = [];
+    for (let i = 0; i < tasks.length; i++){
+      rowsToAdd.push(createNewTaskRow(tasks[i]));
+    }
+    taskContainer.prepend(rowsToAdd)
+
+  }
+
+  function createNewTaskRow(task){
+    const newTaskRow = $([
+      "<span>",
+      task.user_code,
+      task.tasks,
+      "<span>"
+
+
+    ].join("")
+);
+if (tasks.complete){
+  newTaskRow.find("span").css("text-decoration", "line-through")
+}
+return newTaskRow
+  }
+  getTasks();
+
+
+
 
 
 
@@ -44,10 +84,10 @@
   //   let userTask = $("#user-task");
   //   let userFrequency = $("#user-frequency")
 
-  
-  
-  
-  
+
+
+
+
   //   const url = window.location.search;
   //   let taskId;
   //   if (url.indexOf("?task_id=") !== -1) {
@@ -58,7 +98,7 @@
   //   else {
   //     getTasks();
   //   }
-  
+
   //   function getTasks(task) {
   //     taskId = task || "";
   //     if (taskId) {
@@ -83,15 +123,15 @@
   //     }
   //     blogContainer.append(postsToAdd);
   //   }
-  
-
-    /////////////////carousel//////////////////
-  
-   
-    $('.carousel').carousel();
 
 
-  
+  /////////////////carousel//////////////////
+
+
+  $('.carousel').carousel();
+
+
+
 
 
 });
