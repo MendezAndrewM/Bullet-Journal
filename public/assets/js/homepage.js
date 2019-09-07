f16efe3b2c2a03e7936fcb3ca83d9b4b8259575f
unknown <mendez.andrewm@gmail.com>
2019-09-06 18:36:19 -0700

    Cleaned up the file

--- public/assets/js/homepage.js
+++ public/assets/js/homepage.js
@@ -1,77 +1,30 @@
 
 $(document).ready(function () {
 
+  //////////  Event Handlers  /////////////////////////////////////////////////
+
+  $(document).on("click", "button.delete", deleteGoal);
+  $("#submit-btn").on("click", insertNewGoal);
+  $(document).on("click", "button.complete", markComplete)
+
+
+  //////////  Global Variables  ///////////////////////////////////////////////
   const goalsContainer = $(".goals-container");
   let goals;
 
 
-  $(document).on("click", "button.delete", deleteGoal);
-  // $(document).on("click", "button.complete", toggleComplete);
-
-  console.log("hello")
-
-  function getGoals() {
-    $.get("/api/goals", function (data) {
-      goals = data;
-      console.log(goals)
-      initializeGoalRows();
-      
-    })
-  }
-
-
-
-  $("#submit-btn").on("click", insertNewGoal);
-
-
-  function insertNewGoal(event) {
-    event.preventDefault();
-    // alert("zeb sucks")
-    const goal = {
-      user_code: $("#user-name").val().trim(),
-      goal_name: $("#user-goal").val().trim(),
-      // task_frequency: $("#user-frequency").val().trim(),
-      complete: false
-    }
-    console.log(goal)
-    $.post("/api/goals", goal);
-    $("#user-name").val("");
-    $("#user-goal").val("");
-    // $("#user-frequency").val("");
-  };
+  //////////  Create Buttons  /////////////////////////////////////////////////
 
   function initializeGoalRows() {
     goalsContainer.empty();
     const rowsToAdd = [];
     const button = [];
-    for (let i = 0; i < goals.length; i++){
+    for (let i = 0; i < goals.length; i++) {
       rowsToAdd.push(createNewGoalRow(goals[i]));
       button.push(buttonTest(goals[i]))
     }
     goalsContainer.prepend(rowsToAdd)
-
   }
-  function buttonTest(goal){
-    `<button class='complete btn btn-primary'>${goal.user_code}</button>`
-  }
-
-
-
-  function markComplete(event) {
-    event.stopPropagation();
-    id = $(this).attr("id")
-    $.ajax({
-      method: "PUT",
-      url: `/api/goals/${id}`,
-      data: { id: id, complete: true }
-    }).then(getGoals)
-  }
-
-
-
-  $(document).on("click", "button.complete", markComplete)
-
-
 
   function createNewGoalRow(goal) {
     const newGoalRow = $([
@@ -92,10 +45,46 @@ $(document).ready(function () {
     }
     return newGoalRow
   }
-  getGoals();
+
+  function buttonTest(goal) {
+    `<button class='complete btn btn-primary'>${goal.user_code}</button>`
+  }
 
 
-  function deleteGoal(event){
+  ////////// CRUD Functions ///////////////////////////////////////////////////
+
+  function getGoals() {
+    $.get("/api/goals", function (data) {
+      goals = data;
+      console.log(goals)
+      initializeGoalRows();
+    })
+  }
+
+  function insertNewGoal(event) {
+    event.preventDefault();
+    const goal = {
+      user_code: $("#user-name").val().trim(),
+      goal_name: $("#user-goal").val().trim(),
+      complete: false
+    }
+    console.log(goal)
+    $.post("/api/goals", goal);
+    $("#user-name").val("");
+    $("#user-goal").val("");
+  };
+
+  function markComplete(event) {
+    event.stopPropagation();
+    id = $(this).attr("id")
+    $.ajax({
+      method: "PUT",
+      url: `/api/goals/${id}`,
+      data: { id: id, complete: true }
+    }).then(getGoals)
+  }
+
+  function deleteGoal(event) {
     event.stopPropagation();
     id = $(this).attr("id")
     console.log(id)
@@ -103,122 +92,11 @@ $(document).ready(function () {
       method: "DELETE",
       url: "/api/goals/" + id
     }).then(getGoals)
-
-
   }
- 
 
-
-
-
-
-
-
-
-  //   /////////////////carousel//////////////////
-
-
+  ////////// Initiate goals and carousel  ////////////////////////////////////
   $('.carousel').carousel();
   $('select').formSelect();
-
-
-
-
+  getGoals();
 
 });
-
-
-
-
-
-// // Or with jQuery
-// $(document).ready(function () {
-
-//   const taskContainer = $(".task-container");
-//   $(document).on("click", "button.complete", toggleComplete);
-//   $(document).on("click", "button.delete", deleteTask);
-
-//   let tasks = []
-//   function getTasks() {
-//     $.get("/api/tasks", function (data) {
-//       tasks = data;
-//       console.log(tasks)
-//       initializeTaskRows();
-//     })
-//   }
-
-//   $("#submit-btn").on("click", insertNewTask);
-
-//   function insertNewTask(event) {
-//     event.preventDefault();
-//     const task = {
-//       user_code: $("#user-name").val().trim(),
-//       tasks: $("#user-task").val().trim(),
-//       task_frequency: $("#user-frequency").val().trim(),
-//       complete: false
-//     }
-//     $.post("/api/tasks", task);
-//     $("#user-name").val("");
-//     $("#user-task").val("");
-//     $("#user-frequency").val("");
-//   };
-
-//   function initializeTaskRows() {
-//     taskContainer.empty();
-//     const rowsToAdd = [];
-//     for (let i = 0; i < tasks.length; i++) {
-//       rowsToAdd.push(createNewTaskRow(tasks[i]));
-//     }
-//     taskContainer.prepend(rowsToAdd)
-//   }
-
-//   function createNewTaskRow(task) {
-//     const newTaskRow = $([
-//       "<p>",
-//       task.user_code,
-//       "</p>",
-//       "<span>",
-//       task.tasks,
-//       "</span>",
-//       "<button class='complete btn btn-primary'>✓</button>",
-//       "<button class='delete btn btn-danger'>x</button>"
-//     ].join("")
-//     );
-
-//     newTaskRow.find("button.delete").data("id", task.id);
-//     if (task.complete) {
-//       newTaskRow.find("span").css("text-decoration", "line-through")
-//     }
-//     return newTaskRow
-//   }
-
-//   function updateTask(task) {
-//     $.ajax({
-//       method: "PUT",
-//       url: "/api/tasks",
-//       data: task
-//     }).then(getTasks)
-//   }
-
-//   function toggleComplete(event) {
-//     event.stopPropagation();
-//     const task = $(this).parent().data();
-//     console.log(task)
-//     task.complete = !task.complete;
-//     updateTask(task);
-//   }
-
-//   function deleteTask(event) {
-//     event.stopPropagation();
-//     const id = $(this).data("id");
-//     $.ajax({
-//       method: "DELETE",
-//       url: "/api/tasks/" + id
-//     }).then(getTasks);
-//   }
-
-//   getTasks();
-
-//   $('.carousel').carousel();
-
-// });
